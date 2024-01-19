90f02bbab65fbd5ce3be88a441cbeef7b5b85c52
Eduardo Maksoud <eduardobm@tecgraf.puc-rio.br>
2024-01-19 19:23:29 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoAula01/CommandHandler.cs
@@ -0,0 +1,85 @@
+﻿using ProjetoAula01.Entities;
+
+class CommandHandler
+{
+    private readonly UserManager userManager;
+
+    public CommandHandler(UserManager userManager)
+    {
+        this.userManager = userManager;
+    }
+
+    public void ProcessCommand(string command)
+    {
+        string[] commandParts = command.Split(' ');
+
+        switch (commandParts[0].ToLower())
+        {
+            case "getusers":
+                DisplayUsers();
+                break;
+            case "adduser":
+                AddUser();
+                break;
+            case "deleteuser":
+                if (commandParts.Length > 1)
+                {
+                    string userIdString = commandParts[1];
+                    DeleteUserById(userIdString);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command format. Usage: deleteUser <userId>");
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid command. Try again.");
+                break;
+        }
+    }
+
+    private void DisplayUsers()
+    {
+        Console.WriteLine("List of users:");
+        foreach (var user in userManager.GetUsers())
+        {
+            user.Print();
+        }
+    }
+
+    private void AddUser()
+    {
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();
+
+        Console.Write("Enter email: ");
+        string email = Console.ReadLine();
+
+        Console.Write("Enter cpf: ");
+        string cpf = Console.ReadLine();
+
+        User newUser = new User(Guid.NewGuid(), username, email, cpf);
+        userManager.AddUser(newUser);
+
+        Console.WriteLine($"User {newUser.Name} added successfully!");
+    }
+
+    private void DeleteUserById(string userIdString)
+    {
+        if (Guid.TryParse(userIdString, out Guid userId) || Guid.TryParseExact(userIdString, "D", out userId))
+        {
+            if (userManager.DeleteUserById(userId))
+            {
+                Console.WriteLine($"User with ID {userId} deleted successfully.");
+            }
+            else
+            {
+                Console.WriteLine($"User with ID {userId} not found.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid GUID format. Usage: deleteUser <userId>");
+        }
+    }
+}
---
+++ ProjetoAula01/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ProjetoAula01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            UserRepository userRepository = new UserRepository();
+            UserManager userManager = new UserManager(userRepository);
+            CommandHandler commandHandler = new CommandHandler(userManager);
+
+            Console.WriteLine("Type 'getUsers' to retrieve the list of users.");
+            Console.WriteLine("Type 'addUser' to add a new user.");
+            Console.WriteLine("Type 'deleteUser' to delete a user by ID.");
+            Console.WriteLine("Type 'exit' to exit.");
+
+            while (true)
+            {
+                Console.Write("Enter command: ");
+                string command = Console.ReadLine();
+
+                if (command.ToLower() == "exit")
+                {
+                    break;
+                }
+
+                commandHandler.ProcessCommand(command);
+            }
+
+        }
+    }
+}
